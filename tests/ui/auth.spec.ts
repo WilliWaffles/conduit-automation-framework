c226e4076680c65e9d244c5c0e4f2ba12b4a34d2
WilliWaffles <willi_vs1997@hotmail.com>
2026-06-26 00:22:38 -0600

    refactor: restructure tests into ui and api folders

---
+++ tests/ui/auth.spec.ts
@@ -0,0 +1,45 @@
+import { expect } from '@playwright/test';
+import { LoginPage } from '../../pages/LoginPage';
+import { test } from '../../fixtures/auth.fixtures';
+
+test('User can login with correct credentials', { tag: ['@smoke', '@regression'] }, async ({page, registeredUser}) => {
+    const loginPage = new LoginPage(page);
+    
+    // Step 1: Go to Sign in page
+    await loginPage.goToLoginPage();
+
+    await expect(page).toHaveURL("https://demo.realworld.show/login");
+
+    // Step 2: Assert Sign In button, then Add credentials
+    await expect(loginPage.signInBtn).toBeDisabled();
+    await loginPage.fillCredentials(registeredUser.email, registeredUser.password);
+    await expect(loginPage.signInBtn).toBeEnabled();
+
+    // Step 3: Submit info and Verify login
+    await loginPage.loginAndWaitForHome();
+
+    await expect(page).toHaveURL("https://demo.realworld.show/");
+    await expect(page.getByRole("link", {name: registeredUser.username})).toBeVisible();
+});
+
+test('User cannot login with invalid credentials', { tag: '@regression'}, async ({page}) => {
+    const loginPage = new LoginPage(page);
+
+    // Step 1: Go to Sign in page
+    await loginPage.goToLoginPage();
+
+    // Step 2: Assert Sign In button, then Add invalid credentials
+    const INVALID_TEST_EMAIL = "qa-non-existing-email@invalid.com";
+    const TEST_PASSWORD = "741852.";
+
+    await expect(loginPage.signInBtn).toBeDisabled();
+
+    await loginPage.fillCredentials(INVALID_TEST_EMAIL, TEST_PASSWORD);
+    await expect(loginPage.signInBtn).toBeEnabled();
+
+    // Step 3: Submit info and Verify error message
+    await loginPage.submitAndWaitForErrorMsg();
+
+    await expect(loginPage.invalidCredMsg).toBeVisible();
+    await expect(page).toHaveURL("https://demo.realworld.show/login");
+});
---
+++ tests/ui/signUp.spec.ts
@@ -0,0 +1,25 @@
+import {test, expect} from '@playwright/test'
+import { RegisterPage } from '../../pages/RegisterPage';
+import { createUser } from '../../utils/userFactory';
+
+test('User can create an account', { tag: '@regression' }, async ({page}) => {
+    const registerPage = new RegisterPage(page);
+
+    // Step 1: Go to sign up page
+    await registerPage.goToSignUpPage();
+
+    await expect(page).toHaveURL("https://demo.realworld.show/register");
+
+    // Step 2: Assert Sign up button and add credentials
+    const user = createUser();
+
+    await expect(registerPage.signUpBtn).toBeDisabled();
+    await registerPage.fillCredentials(user.username, user.email, user.password);
+    await expect(registerPage.signUpBtn).toBeEnabled();
+
+    // Step 3: Submit info and Verify login
+    await registerPage.submit();
+
+    await expect(page).toHaveURL("https://demo.realworld.show/");
+    await expect(page.getByRole("link", {name: user.username})).toBeVisible();
+});
