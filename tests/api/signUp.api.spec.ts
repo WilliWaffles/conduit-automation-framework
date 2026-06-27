678e737f39a94e06ea9c377cf2d57952af0ba1b6
WilliWaffles <willi_vs1997@hotmail.com>
2026-06-26 23:04:42 -0600

    feat: add negative test for missing password on user registration

--- tests/api/signUp.api.spec.ts
+++ tests/api/signUp.api.spec.ts
@@ -1,5 +1,6 @@
-import { test, expect } from '@playwright/test';
+import { expect } from '@playwright/test';
 import { createUser, type User } from '../../utils/userFactory';
+import { test } from '../../fixtures/api.fixtures';
 
 test('User creation request successful', async ({ request }) => {
     const userData: User = createUser();
@@ -18,3 +19,17 @@ test('User creation request successful', async ({ request }) => {
     expect(responseJson).toHaveProperty('user');
     expect(responseJson.user).toHaveProperty('token');
 });
+
+test('User creation request fails due missing password', async ({ request }) => {
+    const userData: User = createUser();
+    const response = await request.post('https://api.realworld.show/api/users', {
+        data: {
+            user: {
+            username: userData.username,
+            email: userData.email,
+            }
+        }
+    })
+
+    expect(response.status()).toBe(422);
+})
