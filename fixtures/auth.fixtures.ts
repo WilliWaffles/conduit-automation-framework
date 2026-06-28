e1dd690cb6093c3c4dee1ef4bffc5639b202b5f5
WilliWaffles <willi_vs1997@hotmail.com>
2026-06-28 17:20:38 -0600

    feat: add article test suite (publish, blank, edit, delete)

--- fixtures/auth.fixtures.ts
+++ fixtures/auth.fixtures.ts
@@ -3,27 +3,38 @@ import { RegisterPage } from '../pages/RegisterPage';
 import { createUser, User } from '../utils/userFactory';
 
 
-export const test = base.extend<{ registeredUser: User }>({
+export const test = base.extend<{ registeredUser: User, registeredLoggedUser: User }>({
     // registeredUser: object containing unique username, email and password
     // uniqueness guaranteed by Date.now() in userFactory
     registeredUser: async ({ page }, use) => {
         // createUser() must be called inside the fixture to avoid 
         // shared state between parallel tests
-        const registeredUser = createUser();
+        const userData = createUser();
         const registerPage = new RegisterPage(page);
 
         await registerPage.goToSignUpPage();
-        await registerPage.fullSignUpFlow(registeredUser.username, registeredUser.email, registeredUser.password);
+        await registerPage.fullSignUpFlow(userData.username, userData.email, userData.password);
 
-        await expect(page.getByRole("link", {name: registeredUser.username})).toBeVisible();
+        await expect(page.getByRole("link", {name: userData.username})).toBeVisible();
         
-        // Clear cookies to avoid flakynessssss
+        // Clear cookies so user is not logged in and we can test Login flow
         await page.context().clearCookies();
         await page.evaluate(() => localStorage.clear());
 
         await page.reload();
 
-        await use(registeredUser);
+        await use(userData);
 
     },
+    registeredLoggedUser: async ({ page }, use) => {
+        const userData = createUser();
+        const registerPage = new RegisterPage(page);
+
+        await registerPage.goToSignUpPage();
+        await registerPage.fullSignUpFlow(userData.username, userData.email, userData.password);
+
+        await expect(page.getByRole("link", {name: userData.username})).toBeVisible();
+
+        await use(userData);
+    }
 });
---
+++ tests/ui/article.spec.ts
@@ -0,0 +1,76 @@
+import { expect } from '@playwright/test';
+import { ArticleCreatorPage } from '../../pages/ArticleCreatorPage';
+import { createArticle } from '../../utils/articleFactory';
+import { ArticleEditorPage } from '../../pages/ArticleEditorPage';
+import { test } from '../../fixtures/article.fixtures';
+
+
+test('User can publish a valid article and visualize it', { tag: ['@smoke', '@regression'] }, async ({ page, registeredLoggedUser}) => {
+    const articleData = createArticle();
+    const articleCreatorPage = new ArticleCreatorPage(page);
+    const articleEditorPage = new ArticleEditorPage(page, articleData);
+
+    await articleCreatorPage.goToArticleCreationPage();
+    await expect(page).toHaveURL('https://demo.realworld.show/editor');
+
+    await articleCreatorPage.fillArticleData(articleData.title, articleData.summary, articleData.body, articleData.tags);
+
+    await expect(articleCreatorPage.tags).toHaveCount(articleData.tags.length);
+    await articleCreatorPage.submitValidArticle();
+
+    await expect(page).toHaveURL(/\/article\//);
+    await expect(articleEditorPage.title).toHaveText(articleData.title);
+    await expect(articleEditorPage.body).toHaveText(articleData.body);
+    await expect(articleEditorPage.tags).toHaveCount(articleData.tags.length);
+})
+
+test('User cannot publish an article with incomplete information', { tag: '@regression'}, async ({ page, registeredLoggedUser}) => {
+    const articleCreatorPage = new ArticleCreatorPage(page);
+
+    await articleCreatorPage.goToArticleCreationPage();
+    await expect(page).toHaveURL('https://demo.realworld.show/editor');
+
+    await articleCreatorPage.submitInvalidArticle();
+    await expect(page).toHaveURL("https://demo.realworld.show/editor");
+
+    await expect(articleCreatorPage.titleErrorMsg).toBeVisible();
+    await expect(articleCreatorPage.summaryErrorMsg).toBeVisible();
+    await expect(articleCreatorPage.bodyErrorMsg).toBeVisible();
+
+})
+
+test('User can edit and delete a published article', { tag: '@regression'}, async ({ page, publishedArticle, registeredLoggedUser}) => {
+    const articleEditorPage = new ArticleEditorPage(page, publishedArticle);
+    const articleCreatorPage = new ArticleCreatorPage(page);
+    const newTitle  = "Title Changed";
+    const newSummary  = "Summary Changed";
+    const newBody  = "Body Changed";
+    const newTags:string[]  = ["New", "Tags", "Equal", "Four"];
+
+    await expect(articleEditorPage.editBtn.first()).toBeVisible();
+    await articleEditorPage.editArticle();
+
+    await expect(articleCreatorPage.titleInput).toHaveValue(publishedArticle.title);
+    await expect(articleCreatorPage.summaryInput).toHaveValue(publishedArticle.summary);
+    await expect(articleCreatorPage.bodyInput).toHaveValue(publishedArticle.body);
+    await expect(articleCreatorPage.tags).toHaveCount(publishedArticle.tags.length);
+
+    // Edit Article
+    await articleCreatorPage.deleteTags();
+    await articleCreatorPage.fillArticleData(newTitle, newSummary, newBody, newTags);
+    await articleCreatorPage.submitValidArticle();
+
+    await expect(articleEditorPage.title).toHaveText(newTitle);
+    await expect(articleEditorPage.body).toHaveText(newBody);
+    await expect(articleEditorPage.tags).toHaveCount(newTags.length);
+
+    // Delete Article
+    await articleEditorPage.deleteArticle();
+
+    await expect(page).toHaveURL("https://demo.realworld.show/");
+
+    await page.goto(`https://demo.realworld.show/profile/${registeredLoggedUser.username}`);
+
+    await expect(articleEditorPage.articleLinkProfile).not.toBeVisible();
+    await expect(page.getByText("No articles are here... yet.")).toBeVisible();
+})
