a9d69fbc949adf9bcf91e82ee9a9583a9555e97c
WilliWaffles <willi_vs1997@hotmail.com>
2026-06-28 17:21:33 -0600

    doc: remove comments from auth.spect.ts

--- tests/ui/auth.spec.ts
+++ tests/ui/auth.spec.ts
@@ -5,17 +5,14 @@ import { test } from '../../fixtures/auth.fixtures';
 test('User can login with correct credentials', { tag: ['@smoke', '@regression'] }, async ({page, registeredUser}) => {
     const loginPage = new LoginPage(page);
     
-    // Step 1: Go to Sign in page
     await loginPage.goToLoginPage();
 
     await expect(page).toHaveURL("https://demo.realworld.show/login");
 
-    // Step 2: Assert Sign In button, then Add credentials
     await expect(loginPage.signInBtn).toBeDisabled();
     await loginPage.fillCredentials(registeredUser.email, registeredUser.password);
     await expect(loginPage.signInBtn).toBeEnabled();
 
-    // Step 3: Submit info and Verify login
     await loginPage.loginAndWaitForHome();
 
     await expect(page).toHaveURL("https://demo.realworld.show/");
@@ -25,10 +22,9 @@ test('User can login with correct credentials', { tag: ['@smoke', '@regression']
 test('User cannot login with invalid credentials', { tag: '@regression'}, async ({page}) => {
     const loginPage = new LoginPage(page);
 
-    // Step 1: Go to Sign in page
     await loginPage.goToLoginPage();
 
-    // Step 2: Assert Sign In button, then Add invalid credentials
+
     const INVALID_TEST_EMAIL = "qa-non-existing-email@invalid.com";
     const TEST_PASSWORD = "741852.";
 
@@ -37,7 +33,6 @@ test('User cannot login with invalid credentials', { tag: '@regression'}, async 
     await loginPage.fillCredentials(INVALID_TEST_EMAIL, TEST_PASSWORD);
     await expect(loginPage.signInBtn).toBeEnabled();
 
-    // Step 3: Submit info and Verify error message
     await loginPage.submitAndWaitForErrorMsg();
 
     await expect(loginPage.invalidCredMsg).toBeVisible();
